4313b378b0d065c440cefa691c748997faade349
Rafael Costa <rafaelcmd@gmail.com>
2024-10-06 11:08:04 -0300

    finished important packages

---
+++ 02 - important-packages/02-15 - http-client-with-timeout/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"time"
+)
+
+func main() {
+	c := http.Client{Timeout: time.Millisecond * 500}
+
+	resp, err := c.Get("http://google.com")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	println(string(body))
+}
---
+++ 02 - important-packages/02-16 - working-with-post/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	c := http.Client{}
+	jsonVar := bytes.NewBuffer([]byte(`{"name":"John Doe","age":25}`))
+	resp, err := c.Post("http://httpbin.org/post", "application/json", jsonVar)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	io.CopyBuffer(os.Stdout, resp.Body, nil)
+}
