f2c71bbee22c2ab4a9b97220585d8e35801d137b
Rafael Costa <rafaelcmd@gmail.com>
2024-12-08 18:52:34 -0300

    created middleware example

--- 07-apis/cmd/server/main.go
+++ 07-apis/cmd/server/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/jwtauth"
 	"github.com/rafaelcmd/go-expert/07-apis/configs"
 	"github.com/rafaelcmd/go-expert/07-apis/internal/entity"
 	"github.com/rafaelcmd/go-expert/07-apis/internal/infra/database"
 	"github.com/rafaelcmd/go-expert/07-apis/internal/infra/webserver/handlers"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+	"log"
 	"net/http"
 )
 
@@ -26,15 +28,23 @@ func main() {
 
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+	//Using the Custom LogRequest middleware
+	r.Use(LogRequest)
+	//The Recoverer middleware is used to recover from panics
+	r.Use(middleware.Recoverer)
 
 	productDB := database.NewProduct(db)
 	productHandler := handlers.NewProductHandler(productDB)
 
-	r.Post("/products", productHandler.CreateProduct)
-	r.Get("/products/{id}", productHandler.GetProduct)
-	r.Put("/products/{id}", productHandler.UpdateProduct)
-	r.Delete("/products/{id}", productHandler.DeleteProduct)
-	r.Get("/products", productHandler.FindAllProducts)
+	r.Route("/products", func(r chi.Router) {
+		r.Use(jwtauth.Verifier(config.TokenAuth))
+		r.Use(jwtauth.Authenticator)
+		r.Post("/", productHandler.CreateProduct)
+		r.Get("/{id}", productHandler.GetProduct)
+		r.Put("/{id}", productHandler.UpdateProduct)
+		r.Delete("/{id}", productHandler.DeleteProduct)
+		r.Get("/", productHandler.FindAllProducts)
+	})
 
 	userDB := database.NewUser(db)
 	userHandler := handlers.NewUserHandler(userDB, config.TokenAuth, config.JWTExpiresIn)
@@ -46,4 +56,12 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+}
+
+// LogRequest Middleware function to log requests
+func LogRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL)
+		next.ServeHTTP(w, r)
+	})
 }
